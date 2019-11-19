b568d96ead28c89bf80e65624c2f6ddf0f9dce75
jimmyjames88 <james@athletebook.com>
2019-11-18 23:52:07 -0700

    more gulp build steps -- added inlinesource for css and js

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,16 +4,23 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 let cleanCSS = require('gulp-clean-css');
+var inlinesource = require('gulp-inline-source');
 
+gulp.task('inlinesource', ['sass', 'uglify'], function () {
+    return gulp.src('./resources/index.html')
+        .pipe(inlinesource({
+			rootpath: './'
+		}))
+        .pipe(gulp.dest('./'));
+});
 
-
-gulp.task('connect', function() {
+gulp.task('connect', ['watch'], function() {
 	connect.server({
 		port: 3000
 	});
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', ['inlinesource'], function(){
 	gulp.watch(
 		['./resources/js/*'],
 		['uglify']
@@ -39,4 +46,4 @@ gulp.task('uglify', function () {
 		.pipe(gulp.dest('./assets/js'));
 });
 
-gulp.task('default', ['sass', 'uglify', 'connect', 'watch']);
+gulp.task('default', ['sass', 'uglify', 'inlinesource', 'watch', 'connect']);
